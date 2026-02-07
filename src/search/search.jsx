2bed8cd0c5fc2e521bb4ff3c754ad5ca360512fd
raptor-wrangler <kautzmanseven@gmail.com>
2026-02-07 16:50:10 -0700

    fixed the search page

--- src/search/search.jsx
+++ src/search/search.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 export function Search() {
   return (
     <main className="maintext">
-      <div>search displayed here</div>
+      <div>
+        <h2>Search</h2>
+        <h3>Username_placeholder_1</h3>
+            <p>Find games to add to your favorites!</p>
+            <form className="searchform" action="search.html" method="get">
+                <input className="search" type="text" name="query" placeholder="Search..."></input>
+                <button type="submit"> <img className='svg' src="../../svgs/minimalistic-magnifer-svgrepo-com.svg"></img></button>
+            </form>
+            <table>
+                <tr>
+                    <td><img src="https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw__original/img/xV7oisd3RQ8R-k18cdWAYthHXsA=/0x0/filters:format(jpeg)/pic2419375.jpg" alt="Catan" width="100"></img></td>
+                    <td>Catan</td>
+                    <td><button> <img className='svg' src="../../svgs/heart-svgrepo-com.svg"></img> </button></td>
+                </tr>
+                <tr>
+                    <td><img src="https://cf.geekdo-images.com/S3ybV1LAp-8SnHIXLLjVqA__original/img/IsrvRLpUV1TEyZsO5rC-btXaPz0=/0x0/filters:format(jpeg)/pic1534148.jpg" alt="Pandemic" width="100"></img></td>
+                    <td>Pandemic</td>
+                    <td><button><img className='svg' src="../../svgs/heart-svgrepo-com.svg"></img></button></td>
+                </tr>
+                <tr>
+                    <td><img src="https://cf.geekdo-images.com/ZWJg0dCdrWHxVnc0eFXK8w__original/img/LgzEsQlF3xkSEQLoorc8ntiYiIY=/0x0/filters:format(jpeg)/pic38668.jpg" alt="Ticket to Ride" width="100"></img></td>
+                    <td>Ticket to Ride</td>
+                    <td><button><img className='svg' src="../../svgs/heart-svgrepo-com.svg"></img></button></td>
+                </tr>
+            </table>
+        </div>
     </main>
   );
 }
