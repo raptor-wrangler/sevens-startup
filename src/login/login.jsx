0a5ca3510da6e2372d7c5bf9b1a9186834dac6c2
raptor-wrangler <kautzmanseven@gmail.com>
2026-02-07 13:17:17 -0700

    login page working!

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 export function Login() {
   return (
     <main className="maintext">
-      <div>login displayed here</div>
+      <h2>Login</h2>
+        <body> <p> Welcome! Please join your fellow board gamers by logging in. </p> </body>
+        <form get="get" action="search.html">
+            <div className="login">
+                <span> <img className='svg' src='../../public/svgs/letter-svgrepo-com.svg'></img></span>
+                <input type="email" placeholder="Youremail@here.com"></input>
+            </div>
+            <br></br>
+            <div className="login">
+                <span> <img className='svg' src='../../public/svgs/pen-new-square-svgrepo-com.svg'></img></span>
+                <input type="text" placeholder="Username"></input>
+            </div>
+            <br></br>
+            <div className="login">
+                <span> <img className='svg' src='../../public/svgs/key-minimalistic-square-2-svgrepo-com.svg'></img></span>
+                <input type="password" placeholder="Password"></input>
+            </div>
+            <br></br>
+            <button type="submit">Login</button>
+            <button type="submit">Create</button>
+        </form>
     </main>
   );
 }
