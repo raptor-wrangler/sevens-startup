0ebbcf8c53c7c254edfb6445c1c3b84253dee190
raptor-wrangler <kautzmanseven@gmail.com>
2026-02-24 11:26:25 -0700

    created a search function. Need to populate this at the begining of load somehow

--- src/search/search.jsx
+++ src/search/search.jsx
@@ -4,13 +4,14 @@ export function Search() {
   const [games, setGames] = React.useState([]);
   const [listLength, setListLength] = React.useState(20);
   const [lastLength, setLastLength] = React.useState(0);
+  const [search, setSearch] = React.useState('');
 
   async function populateGames() {
     try {
       const response = await fetch('/games.json');
       const gamesList = await response.json();
       setGames(gamesList.slice(lastLength, listLength));
-      setLastLength(listLength);
+      setLastLength(listLength-1);
     } catch (error) {
       console.error('Error loading games:', error);
     }
@@ -27,16 +28,15 @@ export function Search() {
         <h3>Hello, {localStorage.getItem('userName')}</h3>
             <p>Find games to add to your favorites!</p>
             <form className="searchform" action="search.html" method="get">
-                <input className="search" type="text" name="query" placeholder="Search..."></input>
-                <button className="buttonmain" type="submit"> <img className='svg' src="../../svgs/minimalistic-magnifer-svgrepo-com.svg"></img></button>
+                <input className="search" type="text" name="query" placeholder="Search..." onChange={(e) => setSearch(e.target.value)}></input>
             </form>
             <table>
                 <tbody>
-                    {games && games.map((games, index) => (
-                        <tr key={listLength}>
-                            <td><img className="gameimg" src={games.ImagePath} alt={games.Name} width="100"></img></td>
-                            <td>{games.Name}</td>
-                            <td><button className='buttonmain'> <img className='svg' src="../../svgs/heart-svgrepo-com.svg"></img> </button></td>
+                  {games.filter(game => game.Name.includes(search)).map((game, index) => (
+                    <tr key={index}>
+                        <td><img className="gameimg" src={game.ImagePath} alt={game.Name} width="100"></img></td>
+                        <td>{game.Name}</td>
+                        <td><button className='buttonmain'> <img className='svg' src="../../svgs/heart-svgrepo-com.svg"></img> </button></td>
                         </tr>
                     ))}
                 </tbody>
