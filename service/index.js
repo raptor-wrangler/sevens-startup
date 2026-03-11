5ed0543cec7097db7566dd131c0366cffe36e2eb
raptor-wrangler <kautzmanseven@gmail.com>
2026-03-11 16:32:39 -0600

    FAVORITES WORKS

--- service/index.js
+++ service/index.js
@@ -1,14 +1,17 @@
+const cookieParser = require('cookie-parser');
 const uuid = require('uuid');
 const bcrypt = require('bcryptjs');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cookieParser());
+app.use(express.static('public'));
 
+const authCookieName = 'token';
 const users = [];
+let favorites = [];
+const messages = [];
 
 async function createUser(username, password) {
   const passwordHash = await bcrypt.hash(password, 10);
@@ -21,18 +24,14 @@ async function createUser(username, password) {
   return user;
 }
 
-
-function getUser(field, value) {
-  if (value) {
-    return users.find((user) => user[field] === value);
-  }
-  return null;
+async function getUser(field, value) {
+  if (!value) return null;
+  return users.find((u) => u[field] === value);
 }
 
 // Create a token for the user and send a cookie containing the token
-function setAuthCookie(res, user) {
-  user.token = uuid.v4();
-  res.cookie('token', user.token, {
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
     maxAge: 1000 * 60 * 60 * 24 * 365,
     secure: true,
     httpOnly: true,
@@ -40,52 +39,40 @@ function setAuthCookie(res, user) {
 });
 }
 
-function clearAuthCookie(res, user) {
-  delete user.token;
-  res.clearCookie('token');
-}
-
 // Registration endpoint
-app.post('/api/auth', async (req, res) => {
+app.post('/api/auth/create', async (req, res) => {
   if (await getUser('username', req.body.username)) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
     const user = await createUser(req.body.username, req.body.password);
-    setAuthCookie(res, user);
+    setAuthCookie(res, user.token);
+    console.log(req.cookies);
     res.send({ username: user.username });
   }
 });
 
 //Login endpoint
-app.put('/api/auth', async (req, res) => {
+app.post('/api/auth/login', async (req, res) => {
   const user = await getUser('username', req.body.username);
-  if (user && (await bcrypt.compare(req.body.password, user.password))) {
-    setAuthCookie(res, user);
-    res.send({ username: user.username });
-    return;
-  }
-    res.status(401).send({ msg: 'Account not found' });
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
   });
 
-// Logout endpoint  !!!
-app.delete('/api/auth', async (req, res) => {
-  const token = req.cookies['token'];
-  const user = await getUser('token', token);
+// Logout endpoint  !!! he broke rn
+app.delete('/api/auth/logout', async (req, res) => {
+  const user = await getUser('token', req.cookies[authCookieName]);
   if (user) {
-    clearAuthCookie(res, user);
+    delete user.token;
   }
-  res.status(204).send({});
-});
-
-// Get user endpoint
-app.get('/api/user/me', async (req, res) => {
-  const token = req.cookies['token'];
-  const user = await getUser('token', token);
-  if (user) {
-    res.send({ username: user.username });
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
 });
 
 // Default error handler
@@ -95,13 +82,30 @@ app.use(function (err, req, res, next) {
 
 // Middleware to verify that the user is authorized to call an endpoint
 const verifyAuth = async (req, res, next) => {
-    const user = await findUser('token', req.cookies[authCookieName]);
+    const user = await getUser('token', req.cookies[authCookieName]);
     if (user) {
         next();
     } else {
         res.status(401).send({ msg: 'Unauthorized' });
     }
 };
+
+// Add Favorites
+app.post('/api/user/fav', verifyAuth, (req, res) => {
+  favorites.push(req.body);
+  res.send(favorites);
+});
+
+// Get Favorites
+app.get('/api/user/fav', verifyAuth, (_req, res) => {
+  res.send(favorites);
+});
+
+//Delete Favorites
+app.delete('/api/user/fav', verifyAuth, (req, res) => {
+  favorites = favorites.filter(fav => fav.Name !== req.query.key);
+  res.send(favorites);
+});
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.listen(port, () => {
