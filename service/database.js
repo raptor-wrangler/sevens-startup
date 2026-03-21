266d0a0dbb46495e3a93bceec044d112512a041a
raptor-wrangler <kautzmanseven@gmail.com>
2026-03-20 21:28:22 -0600

    copied an adjusted the db file

---
+++ service/database.js
@@ -0,0 +1,57 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('seven');
+const userCollection = db.collection('user');
+const favCollection = db.collection('favorite');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ username: user.username }, { $unset: { token: 1 } });
+}
+
+async function addFavorite(user, favorite) {
+  return favCollection.user.insertOne(favorite);
+}
+
+function getFavorites(user) {
+  return favCollection.find(user);
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  updateUserRemoveAuth,
+  addFavorite,
+  getFavorites,
+};
